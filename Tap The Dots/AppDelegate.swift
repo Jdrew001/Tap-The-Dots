50f6bc7c65bf6f781f4f4eae80f063a3f3512b0d
Drew Atkison <dtatkison@gmail.com>
2024-12-09 16:34:31 -0600

    Initial Commit

---
+++ Tap The Dots/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Tap The Dots
+//
+//  Created by Drew Atkison on 12/9/24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
